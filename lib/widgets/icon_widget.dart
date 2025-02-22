91b736a3d38ea4b10ec2f233aa93b084c417572a
Lenilk <71206634+Lenilk@users.noreply.github.com>
2025-02-23 03:33:02 +0700

    change function that return widget to stateless widget and move them to widgets components

---
+++ lib/widgets/icon_widget.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class icon_widget extends StatelessWidget {
+  final double radius;
+  const icon_widget({super.key,required this.radius});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+        child: SizedBox.fromSize(
+          size: Size.fromRadius(radius), // Image radius
+          child: Image.asset('icon/icon.jpg', fit: BoxFit.cover),
+        ));;
+  }
+}
